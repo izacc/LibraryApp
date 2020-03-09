8a5859fd883bf9497d3934ec6ff71fc9368758fd
Izacc Lucas <Izacc.casey-lucas01@stclairconnect.ca>
2020-03-09 19:53:19 -0400

    Fixed broken method in BookData

--- app/src/main/java/com/example/libraryapplicationproject/DeliciousBeans/BookData.java
+++ app/src/main/java/com/example/libraryapplicationproject/DeliciousBeans/BookData.java
@@ -7,6 +7,16 @@ public class BookData {
     public String bookURL;
     public int bookID;
     public int bookImage;
+    public String bookCat;
+
+
+    //idk how to display image yet so this is temporary
+    public BookData(String bookName, String bookAuthor, String bookCat) {
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.bookCat = bookCat;
+
+    }
 
     /**
      * @author yonis sheekh
